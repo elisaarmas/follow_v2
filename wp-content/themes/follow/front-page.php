fbbfda474af5832d2ce1c36bc5aefeb211044140
elisaarmas <32557995+elisaarmas@users.noreply.github.com>
2017-10-10 15:22:54 +0800

    added template

---
+++ wp-content/themes/follow/front-page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Front Page
+ */
+?>
+<?php while (have_posts()) : the_post(); ?>
+  <?php //get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
+<div class="row">
+        <div class="col-md-3">.col-md-3</div>
+        <div class="col-md-6">.col-md-6</div>
+        <div class="col-md-3">.col-md-3</div>
+      </div>
